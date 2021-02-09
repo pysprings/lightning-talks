7b66e99a29a9df7132ac966d71860ef3d925142e
Tim McFadden <mtik00@users.noreply.github.com>
2021-02-09 12:27:07 -0700

    better explaination

--- caching/lru_cache.py
+++ caching/lru_cache.py
@@ -4,12 +4,22 @@
 A simple script showing built-in Python caching.
 """
 import time
+from contextlib import contextmanager
 from functools import lru_cache
 
 
-@lru_cache(maxsize=128)
+@contextmanager
+def timeit(calculations):
+    """A simple context manager to print the time it takes to call a function."""
+    t_start = time.time()
+    yield
+    t_end = time.time()
+    print(f"{calculations:_} calculations took {t_end-t_start:.2f} seconds")
+
+
+@lru_cache(maxsize=2)
 def calc_pi(number_of_calculations: int) -> float:
-    """Keep approximating Pi for some number of times."""
+    """Keep approximating Pi some number of times."""
     pi = 0
 
     for i in range(0, number_of_calculations):
@@ -18,35 +28,86 @@ def calc_pi(number_of_calculations: int) -> float:
     return pi
 
 
+def test_one():
+    """
+    Call the `calc_pi` function with the same input multiple times.
+
+    The inputs to `calc_pi` are all the same.  The results will be cached, so
+    only the first call will take measureable time.
+    """
+    calculations = 5_000_000  # High enough to take some time
+
+    with timeit(calculations):
+        calc_pi(calculations)
+
+    with timeit(calculations):
+        calc_pi(calculations)
+
+    with timeit(calculations):
+        calc_pi(calculations)
+
+
+def test_two():
+    """
+    Call the `calc_pi` function with two different inputs.
+
+    The first calls for each input will take time, but subsequent calls will not.
+    """
+    calculations = 5_000_000  # High enough to take some time
+
+    # Call the function with the same input 3 times
+    with timeit(calculations):
+        calc_pi(calculations)
+    with timeit(calculations):
+        calc_pi(calculations)
+    with timeit(calculations):
+        calc_pi(calculations)
+
+    # Now change the input.  Only the first call should take time.
+    calculations = 6_000_000
+
+    with timeit(calculations):
+        calc_pi(calculations)
+    with timeit(calculations):
+        calc_pi(calculations)
+    with timeit(calculations):
+        calc_pi(calculations)
+
+
+def test_three():
+    """
+    Mix up the calculations to go above `maxsize`.
+
+    This shows that previously-cached returns can be removed from cache automatically
+    to prevent the cache from growing indefinitely.
+    """
+    with timeit(5_000_000):
+        calc_pi(5_000_000)
+    with timeit(5_000_000):
+        calc_pi(5_000_000)
+
+    with timeit(6_000_000):
+        calc_pi(6_000_000)
+    with timeit(6_000_000):
+        calc_pi(6_000_000)
+
+    with timeit(7_000_000):
+        calc_pi(7_000_000)
+    with timeit(7_000_000):
+        calc_pi(7_000_000)
+
+    # The first calculation should have been removed.  Call the same function
+    # again, but this time the result will not be cached because we told `lru_cache`
+    # to only cache 2 returns.
+    with timeit(5_000_000):
+        calc_pi(5_000_000)
+
+    # However, 7_000_000 should _still_ be cached since it was just called.
+    with timeit(7_000_000):
+        calc_pi(7_000_000)
+
+
 if __name__ == "__main__":
-    digits = 5_000_000  # Something large enough to measure
-
-    # The first time we calculate Pi, it takes a while:
-    t_start = time.time()
-    calc_pi(digits)
-    t_end = time.time()
-    print(
-        f"The first calculation of {digits:_} digits took {t_end-t_start:.2f} seconds"
-    )
-
-    # All subsequent calls won't take "any" time.
-    t_start = time.time()
-    calc_pi(digits)
-    t_end = time.time()
-    print(f"The next calculation took {t_end-t_start:.2f} seconds")
-
-    t_start = time.time()
-    calc_pi(digits)
-    t_end = time.time()
-    print(f"The next calculation took {t_end-t_start:.2f} seconds")
-
-    t_start = time.time()
-    calc_pi(digits)
-    t_end = time.time()
-    print(f"The next calculation took {t_end-t_start:.2f} seconds")
-
-    # We'll have to wait again if we change the input.
-    t_start = time.time()
-    calc_pi(digits * 2)
-    t_end = time.time()
-    print(f"Calculation of {digits * 2:_} digits took {t_end-t_start:.2f} seconds")
+    test_one()
+    # test_two()
+    # test_three()
